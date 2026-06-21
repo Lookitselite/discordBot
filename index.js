939eb5c2fbb1bdfba0171f261584c05fa7200c03
Lookitselite <lookitselite@gmail.com>
2026-06-21 01:03:47 -0400

    package list update

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ import 'dotenv/config';
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once('clientReady', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
     if (interaction.commandName === 'ping') {
+        log(interaction.commandName);
         await interaction.reply('Pong!');
     }
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+function log(cmd) { 
+    console.log(cmd + " recived")
+}
